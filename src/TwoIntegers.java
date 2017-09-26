ed7f38c73a0d4b0eb5dd3793c1c9d52137ee28e0
bob <bob@APCSA>
2017-09-26 14:34:43 -0700

    Added multiple method

--- src/TwoIntegers.java
+++ src/TwoIntegers.java
@@ -52,10 +52,49 @@ public class TwoIntegers
 	public String arithmetic()
 	{
 		String arithmetic = "";
-		arithmetic = "Sum = " + sum();
-		arithmetic += "Difference = " + difference();
-		arithmetic += "Product = " + product();
-		arithmetic += "Quotient = " + quotient();
+		arithmetic = "Sum = " + sum() + "\n";
+		arithmetic += "Difference = " + difference() + "\n";
+		arithmetic += "Product = " + product() + "\n";
+		arithmetic += "Quotient = " + quotient() + "\n";
 		return arithmetic;
+	}
+	public String compareIntegers()
+	{
+		String compareIntegers = "";
+		if(integer1>integer2)
+		{
+			compareIntegers = integer1 + " is greater than " + integer2;
+		}
+		else
+		{
+			compareIntegers = integer1 + " is less than " + integer2;
+		}
+		return compareIntegers;
+	}
+	public String oddOrEven()
+	{
+		String oddOrEven = "";
+		if(integer1%2==0)
+		{
+			oddOrEven = integer1 + " is Even \n";
+		}
+		else
+		{
+			oddOrEven = integer1 + " is Odd \n";
+		}
+		return oddOrEven;
+	}
+	public String multiple()
+	{
+		String multiple = "";
+		if (integer2%integer1==0)
+		{
+			multiple = "Integer 1 is a multiple of Integer 2";
+		}
+		else 
+		{
+			multiple = "Integer 1 is not a multiple of Integer 2";
+		}
+		return multiple;
 	}
 }
--- src/TwoIntegersRunner.java
+++ src/TwoIntegersRunner.java
@@ -8,8 +8,17 @@ public class TwoIntegersRunner
 		System.out.println("Please enter your integers!");
 		int input1 = input.nextInt();
 		int input2 = input.nextInt();
-		TwoIntegers twoIntegers = new TwoIntegers(1, 2);
+		TwoIntegers twoIntegers = new TwoIntegers(input1, input2);
 		String answer = twoIntegers.arithmetic();
 		System.out.println(answer);
+		TwoIntegers compareIntegers = new TwoIntegers(input1, input2);
+		String comparison = compareIntegers.compareIntegers();
+		System.out.println(comparison);
+		TwoIntegers oddOrEven = new TwoIntegers(input1, input2);
+		String oddOrEvenAnswer = oddOrEven.oddOrEven();
+		System.out.print(oddOrEvenAnswer);
+		TwoIntegers multiple = new TwoIntegers(input1, input2);
+		String multipleanswer = multiple.multiple();
+		System.out.print(multipleanswer);
 	}
 }
